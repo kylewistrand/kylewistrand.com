68f815dbec60f3d712b4ae98019840d1615b517c
Kyle Wistrand <kyle.wistrand@gmail.com>
2019-10-23 22:00:30 -0700

    Added files necessary for hosting on GH-Pages

---
+++ docs/precache-manifest.fc02fc68a3c70ad61bd3ebde99da969d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f95358bf1adf5796a5f3dd73687238b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b3a951b9860c6dd9fbb",
+    "url": "/static/css/main.af4671a5.chunk.css"
+  },
+  {
+    "revision": "17d4af5025a2614615b4",
+    "url": "/static/js/2.c3274927.chunk.js"
+  },
+  {
+    "revision": "1b3a951b9860c6dd9fbb",
+    "url": "/static/js/main.01209f10.chunk.js"
+  },
+  {
+    "revision": "a84fa2fe781967e30914",
+    "url": "/static/js/runtime-main.27411a31.js"
+  }
+]);
